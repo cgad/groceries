a9194b920b6453bac7d58cffd3c561eb2138c667
cgad <35946778+cgad@users.noreply.github.com>
2019-04-28 14:39:40 -0600

    Server setup for Heroku deploy

---
+++ build/precache-manifest.f29538d18050edb612f63551ffe98816.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2dcf8cfab7d6c8173e45678919ab01c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e8a19af046eb6826058",
+    "url": "/static/css/main.c01267ad.chunk.css"
+  },
+  {
+    "revision": "a2c3bb9b560ea5ce8b08",
+    "url": "/static/js/2.d25668a5.chunk.js"
+  },
+  {
+    "revision": "2e8a19af046eb6826058",
+    "url": "/static/js/main.59145043.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
